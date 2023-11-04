218da3f8c54b5f6185ee166e63845daf384856c1
Bettenoch <bettenoch254@gmail.com>
2023-11-04 11:00:07 -0700

    Update default count to 0

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,18 +5,21 @@ class Post < ApplicationRecord
 
   after_save :increment_post_counter
 
-  def top_five__comments
-    comments.order(created_at: :desc).limit(5)
+  # validations
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def top_five_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
   end
 
   # A method to increment the likes_counter attribute.
   # def increment_likes_counter
   #   update(likes_counter: likes_counter + 1)
-  # end
-
-  private
+  # endn
 
   def increment_post_counter
-    author.increment!(:posts_counter)
+    author.increment!(:posts_counter) if author.present?
   end
 end
