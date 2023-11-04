e12dd0af66cc72c7531d095c739d5ea8ca02fdcc
Bettenoch <bettenoch254@gmail.com>
2023-11-04 10:58:14 -0700

    Make default count to 0

---
+++ db/migrate/20231103002316_add_default_values_to_comments_and_likes_counters.rb
@@ -0,0 +1,6 @@
+class AddDefaultValuesToCommentsAndLikesCounters < ActiveRecord::Migration[7.1]
+  def change
+    change_column :posts, :comments_counter, :integer, default: 0
+    change_column :posts, :likes_counter, :integer, default: 0
+  end
+end
