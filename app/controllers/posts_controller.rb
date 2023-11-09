93a7437a799d01e2cbf8d730e71ab51333c7d325
Bettenoch <bettenoch254@gmail.com>
2023-11-09 19:59:16 +0300

    Modify post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,22 @@
 class PostsController < ApplicationController
-  before_action :find_user, only: %i[index show]
-  before_action :find_post, only: [:show]
+  before_action :set_user, only: [:index, :show]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = @user.posts
   end
 
   def show
-    # set in the before_actions
+    @comments = @post.comments
   end
 
   private
 
-  def find_user
+  def set_user
     @user = User.find(params[:user_id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:error] = 'User not found'
-    redirect_to users_path
   end
 
-  def find_post
-    @post = @user.posts.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:error] = 'Post not found'
-    redirect_to user_posts_path(@user)
+  def set_post
+    @post = @user.posts.find_by_id(params[:id])
   end
 end
