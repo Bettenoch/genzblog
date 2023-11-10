99eceb09f6c90947f1a18382ef7c12b2996636db
Bettenoch <bettenoch254@gmail.com>
2023-11-10 22:22:32 +0300

    Add current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def current_user
+      @current_user ||= User.first
+      @current_user
+    end
+    # helper method
+    helper_method :current_user
 end
