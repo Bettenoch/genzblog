9d1e53cd205e9e5c90883104bb413bf06705b8f9
Bettenoch <bettenoch254@gmail.com>
2023-11-16 15:39:49 +0300

    Add sign out method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,15 @@ class UsersController < ApplicationController
     @posts = @user.top_three_recent_posts
   end
 
+  def user_sign_out
+    sign_out(current_user)
+    redirect_to root_path, notice: 'Signed out successfully'
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
   end
+
 end
