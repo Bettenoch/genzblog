3c3e2c05d671527fb153bf0c8004317ed22c662f
Bettenoch <bettenoch254@gmail.com>
2023-11-16 15:44:36 +0300

    Configure sign up and sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   # first user
 
-  def current_user
-    @current_user ||= User.first
-    @current_user
+  def update_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :bio, :photo, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :bio, :photo, :password, :current_password)
+    end
   end
-  # helper method
-  helper_method :current_user
 end
