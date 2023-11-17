20be1a6127362690a6f32fd4dcc5175bb115b353
Bettenoch <bettenoch254@gmail.com>
2023-11-17 12:55:13 +0300

    Add load and authorize.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,38 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
+  before_action :find_post
+
   def new
     @comment = Comment.new
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    @comment.post_id = params[:post_id]
+    @comment = current_user.comments.new(comment_params.merge(post: @post))
+   
     if @comment.save
       flash[:notice] = 'Comment created successfully.'
-      redirect_to user_post_path(@comment.post.author, @comment.post)
+      redirect_to user_post_path(@post.author, @post)
     else
-      @post = Post.find(params[:post_id])
-      redirect_to user_post_path(@post.author, @post), notice: 'comment created successfully'
+      render :new
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    @user = @post.author
+    @comment.destroy
+    redirect_to user_post_path(@user, @post) if @post.save
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:text)
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
   end
 end
