c8feab22596ae1d90d2912240978602a720520b9
Bettenoch <bettenoch254@gmail.com>
2023-11-17 12:49:37 +0300

    Add user requested role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,20 @@ class User < ApplicationRecord
   # validations
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :role, inclusion: { in: %w[admin user], message: '%<value>s is not a valid role [admin, user]' }
+
+  # Set default post counter to 0
+  before_create :default_posts_counter
 
   def top_three_recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
+  end
+
+  def default_posts_counter
+    posts_counter || 0
+  end
+
+  def is?(requested_role)
+    role == requested_role.to_s
   end
 end
