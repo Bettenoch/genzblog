896194f94fe631765d498486a3faffd6a1be401f
albertkantwi <albertkantwi@gmail.com>
2023-11-18 08:45:03 +0300

    add post serializer.

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,7 @@
+class PostSerializer < ActiveModel::Serializer
+
+  attributes :id, :title, :text, :comments_counter, :likes_counter
+  has_many :comments
+  belongs_to :author, class_name: 'User'
+  
+end
