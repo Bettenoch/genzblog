4e6e04c550bbd90ef8f79de7dedafdd99bf22405
albertkantwi <albertkantwi@gmail.com>
2023-11-18 08:45:52 +0300

    Add api routes.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Genzblog
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
